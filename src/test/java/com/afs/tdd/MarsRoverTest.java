0cf883b3da21e05c1a1be8282065db4a03b0ab0e
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-01 23:39:37 +0800

    test: all test cases for init position 00W

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -58,6 +58,7 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("W", marsRover.getDirection());
     }
+
     @Test
     void should_10E_given_init_00E_and_command_M() {
         MarsRover marsRover = new MarsRover("N");
@@ -83,5 +84,31 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
         assertEquals("S", marsRover.getDirection());
+    }
+    @Test
+    void should_n10W_given_init_00W_and_command_M() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("M");
+        assertEquals(-1, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getDirection());
+    }
+
+    @Test
+    void should_00S_given_init_00W_and_command_L() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("L");
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirection());
+    }
+
+    @Test
+    void should_00N_given_init_00W_and_command_R() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("R");
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
