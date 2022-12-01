3edaafbb41af7a61dee9d6b14e6582504306788d
leungpo <polly.leung@oocl.com>
2022-12-01 18:51:17 +0800

    test: add test case should_go_to_0_0_S_when_execute_command_given_0_0_W_and_L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -80,4 +80,16 @@ class MarsRoverTest {
         assertEquals("E",marsRover.getDirection());
     }
 
+    @Test
+    void should_go_to_0_0_S_when_execute_command_given_0_0_W_and_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        //when
+        marsRover.executeCommand("L");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirection());
+    }
+
 }
