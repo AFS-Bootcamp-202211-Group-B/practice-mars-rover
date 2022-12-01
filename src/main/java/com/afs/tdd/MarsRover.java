56d782eb9c3c91fdd798dc0bd999f88c41440a02
tokaho <tokaho2000@gmail.com>
2022-12-01 17:17:13 +0800

    [Feat] implement turnLeft()

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -11,8 +11,58 @@ public class MarsRover {
         this.direction=direction;
     }
 
-    public void executeCommand(String m) {
+    public void executeCommand(String command) {
+        switch(command){
+            case "M":
+                move();
+                break;
+            case "L":
+                turnLeft();
+                break;
+            case "R":
+                break;
+            default:
+                break;
+        }
 
+    }
+
+    private void turnLeft() {
+        switch(this.direction){
+            case "N":
+                this.direction="W";
+                break;
+            case "E":
+                this.direction="N";
+                break;
+            case "S":
+                this.direction="E";
+                break;
+            case "W":
+                this.direction="S";
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void move() {
+        switch(this.direction){
+            case "N":
+                y++;
+                break;
+            case "E":
+                x++;
+                break;
+            case "S":
+                y--;
+                break;
+            case "W":
+                x--;
+                break;
+            default:
+                break;
+        }
     }
 
     public int getLocationX() {
