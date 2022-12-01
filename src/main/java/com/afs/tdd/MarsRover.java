85cb0b690171fd3bfad24eda83afc95e9b3faa5d
tokaho <tokaho2000@gmail.com>
2022-12-01 16:59:23 +0800

    [Test] should_0_1_N_when_execute_command_given_0_0_N_and_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,29 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private int x;
+    private int y;
+    private String direction;
+
+    public MarsRover(int x, int y, String direction) {
+        this.x=x;
+        this.y=y;
+        this.direction=direction;
+    }
+
+    public void executeCommand(String m) {
+
+    }
+
+    public int getLocationX() {
+        return this.x;
+    }
+
+    public int getLocationY() {
+        return this.y;
+    }
+
+    public String getDirection() {
+        return this.direction;
+    }
 }
