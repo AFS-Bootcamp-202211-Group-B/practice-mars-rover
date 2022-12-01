d675d80a53fecaec7ef7f065e813a6c566cb8779
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-01 23:37:07 +0800

    test: all test cases for inital position 00S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -31,4 +31,30 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("E", marsRover.getDirection());
     }
+    @Test
+    void should_0n1S_given_init_00S_and_command_M() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("M");
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirection());
+    }
+
+    @Test
+    void should_00E_given_init_00S_and_command_L() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("L");
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getDirection());
+    }
+
+    @Test
+    void should_00W_given_init_00S_and_command_R() {
+        MarsRover marsRover = new MarsRover("N");
+        marsRover.executeCommand("R");
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
