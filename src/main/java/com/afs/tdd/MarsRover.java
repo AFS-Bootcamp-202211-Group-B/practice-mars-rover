da5c0f39ef679d5b93eee926b4fd17307ceff9d4
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-01 23:14:08 +0800

    feat: declare all functions and class attributes

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,25 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private int locationX = 0;
+    private int locationY = 0;
+    private char direction;
+
+    public MarsRover(char direction){
+        this.direction = direction;
+    }
+    public void executeBatchCommands(String command){
+
+    }
+    private void move(String command){
+        
+    }
+    private String turnLeft(String direction){
+        return "";
+    }
+
+    private String turnRight(String direction){
+        return "";
+    }
+
 }
