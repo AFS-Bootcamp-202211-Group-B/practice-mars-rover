1c36a4a4c48009c902eb76e1049f40fffdc85106
leungpo <polly.leung@oocl.com>
2022-12-01 16:58:25 +0800

    test: add test case should_go_to_0_1_N_when_execute_command_given_0_0_N_and_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,21 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    public MarsRover(int locationX, int locationY, String direction) {
+    }
+
+    public void executeCommand(String command) {
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getDirection() {
+        return "N";
+    }
 }
