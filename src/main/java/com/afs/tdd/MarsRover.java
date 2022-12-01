6a241351cc5728c32927f3bd531ad3a5e31715f9
Antony Choi <antony.choi@oocl.com>
2022-12-01 20:32:54 +0800

    test: should_return_0_1_N_when_executeCommand_given_0_0_N_M

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,20 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+    public MarsRover(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public void executeBatchCommands(String command) {
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,17 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_0_1_N_when_executeCommand_given_0_0_N_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        //when
+        marsRover.executeBatchCommands("M");
+        //then
+        assertEquals(1, marsRover.getLocationX());
     }
+    
 }
